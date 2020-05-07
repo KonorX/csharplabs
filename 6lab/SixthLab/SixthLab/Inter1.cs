93d2fcf539c72e6ecd51586503955fb6f438ebbd
KonorX <61805274+KonorX@users.noreply.github.com>
2020-05-07 11:27:38 +0300

    Add files via upload

---
+++ 6lab/SixthLab/SixthLab/Inter1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SixthLab
+{
+    interface Inter1
+    {
+        public void Assesment();
+    }
+}
---
+++ 6lab/SixthLab/SixthLab/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SixthLab
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Spec { get; set; }
+        protected string ID { get; set; }
+        public int SemAverage { get; set; }
+        public Student()
+        {
+            Name = "Ruslan";
+            Age = 18;
+            Spec = "IiTP";
+            ID = GenId();
+        }
+
+        public Student(string s = " ", int a = 0, string sp = " ")
+        {
+            Name = s;
+            Age = a;
+            Spec = sp;
+            ID = GenId();
+            
+        }
+
+        protected static string GenId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+        public override string ToString()
+        {
+            return ($"Имя:{Name}\t Возраст:{Age}\t Специальность:{Spec}\t ID:{ID}\t");
+        }
+        
+    }
+}
