24dd644b939e25ef558c3139056c3c83792ac005
KonorX <61805274+KonorX@users.noreply.github.com>
2020-05-09 20:09:28 +0300

    Add files via upload

---
+++ 7lab/SeventhLab/SeventhLab/OverloadMethods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeventhLab
+{
+    class OverloadMethods
+    {
+        public int number { get; set; }
+
+        public static int IntegCheck()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Не подходит,введи ещё раз  ");
+            }
+            return a;
+        }
+
+        public static int NCheck()
+        {
+            int b;
+            while ((!int.TryParse(Console.ReadLine(), out b) || b <= 0))
+            {
+                Console.WriteLine("Не подходит,введи ещё раз  ");
+            }
+            return b;
+        }
+
+        public static string StrRepresent(string znach, int a)
+        {
+            string s;
+            s = znach + $"={a}";
+            return s;
+        }
+
+        public static OverloadMethods operator +(OverloadMethods a,OverloadMethods b)
+        {
+            return new OverloadMethods { number = a.number + b.number };
+        }
+        public static OverloadMethods operator -(OverloadMethods a,OverloadMethods b)
+        {
+            return new OverloadMethods { number = a.number - b.number };
+        }
+        public static OverloadMethods operator *(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { number = a.number * b.number };
+        }
+        public static OverloadMethods operator /(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { number = a.number / b.number };
+        }
+        public static OverloadMethods operator %(OverloadMethods a,OverloadMethods b)
+        {
+            return new OverloadMethods { number = a.number % b.number };
+        }
+        /*public static OverloadMethods operator ^(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { number = (int)System.Math.Pow(a.number, b.number) };
+        }*/
+        public static bool operator >(OverloadMethods a,OverloadMethods b)
+        {
+            return a.number > b.number;
+        }
+        
+        public static bool operator <(OverloadMethods a,OverloadMethods b)
+        {
+            return a.number < b.number;
+        }
+        
+        public static bool operator ==(OverloadMethods a, OverloadMethods b)
+        {
+            return a.number == b.number;
+        }
+        public static bool operator !=(OverloadMethods a, OverloadMethods b)
+        {
+            return a.number != b.number;
+        }
+
+    }
+}
---
+++ 7lab/SeventhLab/SeventhLab/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SeventhLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            Console.WriteLine("Введи целое число(N)");
+            n = OverloadMethods.IntegCheck();
+            Console.WriteLine("Введи натуральное число(M)  (>0)  ");
+            m = OverloadMethods.NCheck();
+            OverloadMethods N = new OverloadMethods { number = n };
+            OverloadMethods M = new OverloadMethods { number = m };
+            Console.WriteLine($"N+M= {(N + M).number}");
+            Console.WriteLine($"N-M={(N-M).number}");
+            Console.WriteLine($"N*M={(N*M).number}");
+            Console.WriteLine($"N/M={(N / M).number}");
+            Console.WriteLine($"N%M={(N%M).number}");
+            //Console.WriteLine($"N^M={(N^M).number}");
+            bool eq;
+            eq = N == M;
+            switch (eq)
+            {
+                case true:
+                    Console.WriteLine($"{n}={m}");
+                    break;
+                case false:
+                    Console.WriteLine($"{n}!={m}");
+                    bool bm;
+                    bm = N > M;
+                    switch (bm)
+                    {
+                        case true:
+                            Console.WriteLine($"{n}>{m}");
+                            break;
+                        case false:
+                            Console.WriteLine($"{n}<{m}");
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}
