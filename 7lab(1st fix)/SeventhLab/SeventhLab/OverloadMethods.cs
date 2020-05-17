2acfdb216aabd2c5eb840894cba6c4749cef6d69
KonorX <61805274+KonorX@users.noreply.github.com>
2020-05-17 18:09:09 +0300

    Add files via upload

---
+++ 7lab(1st fix)/SeventhLab/SeventhLab/OverloadMethods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeventhLab
+{
+    class OverloadMethods
+    {
+        public int Number { get; set; }
+
+        public static int CheckingInteger()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Не подходит,введи ещё раз  ");
+            }
+            return a;
+        }
+
+        public static int CheckingNatural()
+        {
+            int b;
+            while ((!int.TryParse(Console.ReadLine(), out b) || b <= 0))
+            {
+                Console.WriteLine("Не подходит,введи ещё раз  ");
+            }
+            return b;
+        }
+
+        public static string RepresentInString(string znach, int a)
+        {
+            string s;
+            s = znach + $"={a}";
+            return s;
+        }
+
+        public static OverloadMethods operator +(OverloadMethods a,OverloadMethods b)
+        {
+            return new OverloadMethods { Number = a.Number + b.Number };
+        }
+        public static OverloadMethods operator -(OverloadMethods a,OverloadMethods b)
+        {
+            return new OverloadMethods { Number = a.Number - b.Number };
+        }
+        public static OverloadMethods operator *(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { Number = a.Number * b.Number };
+        }
+        public static OverloadMethods operator /(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { Number = a.Number / b.Number };
+        }
+        public static OverloadMethods operator %(OverloadMethods a,OverloadMethods b)
+        {
+            return new OverloadMethods { Number = a.Number % b.Number };
+        }
+        /*public static OverloadMethods operator ^(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { number = (int)System.Math.Pow(a.number, b.number) };
+        }*/
+        public static bool operator >(OverloadMethods a,OverloadMethods b)
+        {
+            return a.Number > b.Number;
+        }
+        
+        public static bool operator <(OverloadMethods a,OverloadMethods b)
+        {
+            return a.Number < b.Number;
+        }
+        
+        public static bool operator ==(OverloadMethods a, OverloadMethods b)
+        {
+            return a.Number == b.Number;
+        }
+        public static bool operator !=(OverloadMethods a, OverloadMethods b)
+        {
+            return a.Number != b.Number;
+        }
+
+    }
+}
---
+++ 7lab(1st fix)/SeventhLab/SeventhLab/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SeventhLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            Console.WriteLine("Введи целое число(N)");
+            n = OverloadMethods.CheckingInteger();
+            Console.WriteLine("Введи натуральное число(M)  (>0)  ");
+            m = OverloadMethods.CheckingNatural();
+            OverloadMethods N = new OverloadMethods { Number = n };
+            OverloadMethods M = new OverloadMethods { Number = m };
+            Console.WriteLine(OverloadMethods.RepresentInString("первое число", n));
+            Console.WriteLine(OverloadMethods.RepresentInString("второе число", m));
+            Console.WriteLine($"N+M= {(N + M).Number}");
+            Console.WriteLine($"N-M={(N-M).Number}");
+            Console.WriteLine($"N*M={(N*M).Number}");
+            Console.WriteLine($"N/M={(N / M).Number}");
+            Console.WriteLine($"N%M={(N%M).Number}");
+            //Console.WriteLine($"N^M={(N^M).number}");
+            bool equal;
+            equal = N == M;
+            switch (equal)
+            {
+                case true:
+                    Console.WriteLine($"{n}={m}");
+                    break;
+                case false:
+                    Console.WriteLine($"{n}!={m}");
+                    bool bm;
+                    bm = N > M;
+                    switch (bm)
+                    {
+                        case true:
+                            Console.WriteLine($"{n}>{m}");
+                            break;
+                        case false:
+                            Console.WriteLine($"{n}<{m}");
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}
